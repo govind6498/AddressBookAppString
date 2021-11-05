015dbb5107284334bc6f6ef701fe4d2a09025578
govind6498 <govindsingh6498@gmail.com>
2021-11-05 14:13:45 +0530

    Added:To Implement AddressBook Service methids

---
+++ src/main/java/com/example/addressbookapp/service/AddressBookService.java
@@ -0,0 +1,53 @@
+package com.example.addressbookapp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.addressbookapp.dto.AddressBookDTO;
+import com.example.addressbookapp.dto.ResponseDTO;
+import com.example.addressbookapp.model.AddressBookData;
+
+@Service
+public class AddressBookService implements IAddressBookService {
+	
+	private List<AddressBookData> addressBookList =new ArrayList<>();
+	
+	@Override
+	public List<AddressBookData> getAddressBookData() {
+		return addressBookList;
+	}
+
+	@Override
+	public AddressBookData getAddressBookDataById(int addId) {
+		return addressBookList.get(addId-1);
+	}
+
+	@Override
+	public AddressBookData createAddressBookData(AddressBookDTO addressBookDTO) {
+		AddressBookData addressBookData =  null;
+		addressBookData = new AddressBookData(1,addressBookDTO);
+		addressBookList.add(addressBookData);
+		return addressBookData;
+	}
+
+	@Override
+	public AddressBookData updateAddressBookData(int addId, AddressBookDTO addressBookDTO) {
+		AddressBookData addressBookData = this.getAddressBookDataById(addId);
+		addressBookData.setName("Himanshu");
+		addressBookData.setMobileNumber("9685743620");
+		addressBookData.setCity("Delhi");
+		addressBookData.setState("New Delhi");
+		addressBookData.setEmail("abc123@gmail.com");
+		addressBookList.set(addId-1, addressBookData);
+		return addressBookData;
+	}
+
+	@Override
+	public void deleteAddressBookData(int addId) {
+		addressBookList.remove(addId-1);
+		
+	}
+
+}
