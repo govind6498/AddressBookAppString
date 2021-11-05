49e59d7a69b8c94669b4c84990f5847dd7daef20
govind6498 <govindsingh6498@gmail.com>
2021-11-05 12:47:59 +0530

    Added:To Create a dto that stores response

---
+++ src/main/java/com/example/addressbookapp/dto/ResponseDTO.java
@@ -0,0 +1,26 @@
+package com.example.addressbookapp.dto;
+
+public class ResponseDTO {
+	private String message;
+	private Object data;
+	
+	public ResponseDTO(String message,Object data) {
+		this.message = message;
+		this.data = data;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMesage(String message) {
+		
+		this.message = message;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+}
