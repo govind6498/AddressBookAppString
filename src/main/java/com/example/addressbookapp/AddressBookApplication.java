b3a4c66343c5a63f2fe05d8df3d5378883091582
govind6498 <govindsingh6498@gmail.com>
2021-11-06 01:04:59 +0530

    Added:To Start logging using Slf4j provied by lombok

--- src/main/java/com/example/addressbookapp/AddressBookApplication.java
+++ src/main/java/com/example/addressbookapp/AddressBookApplication.java
@@ -2,12 +2,16 @@ package com.example.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
-public class AddressBookAppApplication {
-
+@Slf4j
+public class AddressBookApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookAppApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookApplication.class,args);
+		log.info("Address Book App Stated in {} Environment",context.getEnvironment().getProperty("environmen"));
 	}
 
 }
